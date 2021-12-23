f91234be4a3c0afd1b3b3ef988fe50a0c61a8da4
Yuriy Polyakevich <yuriy.polyakevich@tensor.technology>
2021-12-23 23:06:13 +0300

    points segment amount

--- src/com/company/PointInSegmentAmount.java
+++ src/com/company/PointInSegmentAmount.java
@@ -1,109 +1,52 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-/** на оси ох заданы отрезки.
-  * x1[] - левые концы, x2[] - правые концы - arrays of ints.
-  * потом идут запросы - каждый запрос - одна точка на ох (double). нужно ответить во сколько отрезков она входит.
-  * входной файл
-  * N, M — кол-во отрезков, кол-во запросов
-  * x1_0, ... x1_N
-  * x2_0, ... x2_N
-  * q_x_1
-  * q_x_2
-  * ...
-  * q_x_M
+/**
+ * на оси ох заданы отрезки.
+ * x1[] - левые концы, x2[] - правые концы - arrays of ints.
+ * потом идут запросы - каждый запрос - одна точка на ох (double). нужно ответить во сколько отрезков она входит.
+ * входной файл
+ * N, M — кол-во отрезков, кол-во запросов
+ * x1_0, ... x1_N
+ * x2_0, ... x2_N
+ * q_x_1
+ * q_x_2
+ * ...
+ * q_x_M
  */
 public class PointInSegmentAmount implements Task {
     @Override
     public Object solveTask(Object... args) {
-        int x[] = (int[]) args[0];
-        int y[] = (int[]) args[1];
+        Integer x[] = (Integer[]) args[0];
+        Integer y[] = (Integer[]) args[1];
         double q[] = (double[]) args[2];
         int result[] = new int[q.length];
-        int max = y[y.length - 1];
-        int n = q.length > x.length ? q.length : x.length;
-        for (int i = 0; i < n; i++) {
-            if (i < x.length - 1) {
-                for (int j = i + 1; j < x.length; j++) {
-                    if (x[j] < x[i]) {
-                        int t = x[j];
-                        x[j] = x[i];
-                        x[i] = t;
-                        int t1 = y[j];
-                        y[j] = y[i];
-                        y[i] = t1;
-                    }
-                }
-            }
-            if (i < q.length) {
-                for (int j = i + 1; j < q.length; j++) {
-                    if (q[j] < q[i]) {
-                        double t = q[j];
-                        q[j] = q[i];
-                        q[i] = t;
-                    }
-                }
-            }
-            if (max < y[i]) {
-                max = y[i];
-            }
-        }
-        int i = x[0];
-        int exist[] = new int[x.length];
-        int xIndex = 0;
-        int qIndex = 0;
-        while (q[qIndex] < x[0]) {
-            result[qIndex] = 0;
-            qIndex++;
-        }
-        if (qIndex == result.length) {
-            return result;
-        }
+
+        Integer[] indexes = IntStream.range(0, x.length).boxed().toArray(Integer[]::new);
+        Comparator<Integer> xComp = Comparator.comparingInt(a -> x[a]);
+        Arrays.sort(indexes, 0, indexes.length, xComp);
+
+        int i = x[indexes[0]];
+        int max = Collections.max(Arrays.asList(x), null);
         final List<Integer> indexesToCheck = new ArrayList<>();
-        while (i <= max) {
-            if (qIndex < q.length && (q[qIndex] < i && q[qIndex] > (i - 1))) {
-                final long count = Arrays.stream(exist).filter(e -> e == 1).count();
-                while (qIndex < q.length && (q[qIndex] < i && q[qIndex] > (i - 1))) {
-                    result[qIndex] = (int) count;
-                    qIndex++;
-                }
-            }
 
-            while (xIndex < x.length && x[xIndex] == i) {
-                exist[xIndex] = 1;
-                indexesToCheck.add(xIndex);
-                xIndex++;
-            }
+        for (int j = i; j <= max; j++) {
 
-            if (qIndex < q.length && (q[qIndex] == i)) {
-                final long count = Arrays.stream(exist).filter(e -> e == 1).count();
-                while (qIndex < q.length && (q[qIndex] <= i && q[qIndex] > (i - 1))) {
-                    result[qIndex] = (int) count;
-                    qIndex++;
-                }
-            }
-
-            for (int j = 0; j < indexesToCheck.size(); j++) {
-                final Integer integer = indexesToCheck.get(j);
-                if (y[integer] == i) {
-                    exist[integer] = 0;
-                    indexesToCheck.remove(integer);
-                    j--;
-                }
-            }
-            i++;
         }
-        if (qIndex != result.length) {
-            for (int j = qIndex; j < result.length; j++) {
-                result[j] = 0;
-            }
-        }
+
         for (int j = 0; j < result.length; j++) {
             System.out.println(String.format("Point: {%s}, amount: {%s}", q[j], result[j]));
         }
         return result;
+    }
+
+    public class TimeUnit {
+        int fromIncluded;
+        int toExcluded;
+        int number;
     }
 }
