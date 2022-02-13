feb06d0877e42498624862643b2bf6e5a2987d51
YuriyPolyakevich <yury.paliakevich.extern@gulp.de>
2022-02-13 18:11:29 +0300

    K_AscendingElementInSubArray

---
+++ src/com/company/google/HeyI_AlreadyDidThat.java
@@ -0,0 +1,106 @@
+package com.company.google;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by yuriy_polyakevich at 2/13/22
+ */
+
+//Hey, I Already Did That!
+//========================
+//
+//Commander Lambda uses an automated algorithm to assign minions randomly to tasks, in order to keep minions on their toes.
+//But you've noticed a flaw in the algorithm --
+//it eventually loops back on itself,
+//so that instead of assigning new minions as it iterates,
+//it gets stuck in a cycle of values so that the same minions end up
+//doing the same tasks over and over again. You think proving this to Commander
+//Lambda will help you make a case for your next promotion.
+//
+//You have worked out that the algorithm has the following process:
+//
+//1) Start with a random minion ID n, which is a nonnegative integer of length k in base b
+//2) Define x and y as integers of length k.  x has the digits of n in descending order, and y has the digits of n in ascending order
+//3) Define z = x - y.  Add leading zeros to z to maintain length k if necessary
+//4) Assign n = z to get the next minion ID, and go back to step 2
+//
+//For example, given minion ID n = 1211, k = 4, b = 10, then x = 2111, y = 1112 and z = 2111 - 1112 = 0999.
+//Then the next minion ID will be n = 0999 and the algorithm
+//iterates again: x = 9990, y = 0999 and z = 9990 - 0999 = 8991, and so on.
+//
+//Depending on the values of n, k (derived from n), and b,
+//at some point the algorithm reaches a cycle, such as by reaching a constant value.
+//For example, starting with n = 210022, k = 6, b = 3,
+//the algorithm will reach the cycle of values [210111, 122221, 102212] and it will
+//stay in this cycle no matter how many times it continues iterating. Starting with n = 1211,
+//the routine will reach the integer 6174, and since 7641 - 1467 is 6174, it will stay
+//as that value no matter how many times it iterates.
+//
+//Given a minion ID as a string n representing a nonnegative integer
+//of length k in base b, where 2 <= k <= 9 and 2 <= b <= 10, write a
+//function solution(n, b) which returns the length of the ending cycle of the
+//algorithm above starting with n. For instance, in the example above,
+//solution(210022, 3) would return 3, since iterating on 102212 would return to
+//210111 when done in base 3. If the algorithm reaches a constant, such as 0, then the length is 1.
+//
+//Languages
+//=========
+//
+//To provide a Java solution, edit Solution.java
+//To provide a Python solution, edit solution.py
+//
+//Test cases
+//==========
+//Your code should pass the following test cases.
+//Note that it may also be run against hidden test cases not shown here.
+//
+//-- Java cases --
+//Input:
+//Solution.solution('210022', 3)
+//Output:
+//    3
+//
+//Input:
+//Solution.solution('1211', 10)
+//Output:
+//    1
+//
+//-- Python cases --
+//Input:
+//solution.solution('1211', 10)
+//Output:
+//    1
+//
+//Input:
+//solution.solution('210022', 3)
+//Output:
+//    3
+public class HeyI_AlreadyDidThat {
+
+    public static void main(String[] args) {
+        System.out.println(solution("1211", 10));
+    }
+
+    //1) Start with a random minion ID n, which is a nonnegative integer of length k in base b
+    //2) Define x and y as integers of length k.  x has the digits of n in descending order, and y has the digits of n in ascending order
+    //3) Define z = x - y.  Add leading zeros to z to maintain length k if necessary
+    //4) Assign n = z to get the next minion ID, and go back to step 2
+    public static int solution(String n, int b) {
+        final char[] chars = n.toCharArray();
+        Arrays.sort(chars);
+        final String y = String.copyValueOf(chars, 0, chars.length);
+        final Character[] arr = n.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
+        Arrays.sort(arr, Comparator.reverseOrder());
+        final String x = Arrays.stream(arr).map(Objects::toString).collect(Collectors.joining());
+
+        System.out.println(x);
+        System.out.println(y);
+        //Your code here
+        return 0;
+    }
+}
--- src/com/company/yuriy/K_AscendingElementInSubArray.java
+++ src/com/company/yuriy/K_AscendingElementInSubArray.java
@@ -1,6 +1,9 @@
 package com.company.yuriy;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -8,7 +11,7 @@ import java.util.Queue;
  * Created by yuriy_polyakevich at 2/6/22
  */
 
-//По массиву двигаются два указателя — Тит, которые изначально направлены на самый первый элемент массива.
+//По массиву двигаются два указателя — L и R, которые изначально направлены на самый первый элемент массива.
 //Есть две операции: L— сдвинуть указатель 1 на один элемент вправо и R —
 //сдвинуть указатель г на один элемент вправо. После выполнения каждой из операций нужно вывести
 //k -ев порядке возрастания число среди всех чисел от 1 до е включительно, либо -1, если всего чисел от I до г меньше, чем k.
@@ -16,7 +19,7 @@ import java.util.Queue;
 //В первой строке входа — три целых числа п, т, k.
 //Во второй строке п целых чисел, задающих массив чисел, по которому будут двигаться два указателя, 1 ит.
 //В третьей строке входного файла — т символов R или L, без пробелов, в одну строку.
-//Это порядок выполняемых операций. Гарантируется, что указатель I никогда не “обгоняет" указатель т.
+//Это порядок выполняемых операций. Гарантируется, что указатель L никогда не “обгоняет" указатель R.
 //Гарантируется, что указатель к никогда не выйдет за пределы массива. При этом 1<n, k< 100000, 0 < m < 2n – 2.
 //Все числа в массиве неотрицательные и не превосходят 10°.
 //Формат вывода
@@ -33,39 +36,99 @@ import java.util.Queue;
 public class K_AscendingElementInSubArray {
 
     public static void main(String[] args) {
-        final int k = 2;
-        final Integer[] arr = { 4, 2, 1, 3, 6, 5, 7 };
-        final Character[] movings = { 'R', 'R', 'L', 'L'};
+        //        final int k = 2;
+        //        final Integer[] arr = { 4, 2, 1, 3, 6, 5, 7 };
+        //        final Character[] movings = { 'R', 'R', 'L', 'L' };
+        final int k = 5_000;
+        final Character[] movings = new Character[100_000];
+        final Integer[] arr = new Integer[1_000_000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        Arrays.fill(movings, 'R');
+        for (int i = movings.length / 2; i < movings.length; i++) {
+            movings[i] = 'L';
+        }
         final long startTime = System.currentTimeMillis();
         solveProblem(arr, k, movings);
         System.out.println("Time spent: " + (System.currentTimeMillis() - startTime));
     }
 
-    private static void solveProblem(Integer[] arr, int k, Character[] movings) {
+    private static void solveProblem(Integer[] arr, int k, Character[] moves) {
         int l = 0, r = 0;
         final StringBuilder result = new StringBuilder();
-        for (final Character move : movings) {
-            if ('R' == move) {
+        final Queue<Value> mainQueue = new PriorityQueue<>((o1, o2) -> -Integer.compare(o1.value, o2.value));
+        final Value initialValue = new Value(0, arr[0], true);
+        mainQueue.add(initialValue);
+        final Queue<Value> nonMainQueue = new PriorityQueue<>(Comparator.comparingInt(value -> value.value));
+        final Map<Integer, Value> valuesMap = new HashMap<>();
+        valuesMap.put(0, initialValue);
+        for (final Character move : moves) {
+            if (move == 'R') {
                 r++;
             } else {
                 l++;
             }
-            if (k > (r - l) + 1) {
-                result.append(-1).append(" ");
-                continue;
+            final int index = r - l;
+            final int newValueInt = arr[index];
+            final Value newValue = new Value(index, newValueInt, true);
+            if ('R' == move) {
+                final boolean mainQueueNotFilled = mainQueue.size() < k;
+                if (mainQueueNotFilled) {
+                    mainQueue.add(newValue);
+                    if (mainQueue.size() == k) {
+                        result.append(mainQueue.peek().value).append(" ");
+                    } else {
+                        result.append("-1").append(" ");
+                    }
+                    valuesMap.put(index, newValue);
+                    continue;
+                } else {
+                    final Value peek = mainQueue.peek();
+                    if (peek.value >= newValue.value) {
+                        final Value poll = mainQueue.poll();
+                        poll.isInMainQueue = false;
+                        nonMainQueue.add(poll);
+                        mainQueue.add(newValue);
+                    } else {
+                        newValue.isInMainQueue = false;
+                        nonMainQueue.add(newValue);
+                    }
+                }
+                valuesMap.put(index, newValue);
+            } else {
+                final Value value = valuesMap.remove(l - 1);
+                if (value.isInMainQueue) {
+                    mainQueue.remove(value);
+                    if (!nonMainQueue.isEmpty()) {
+                        final Value poll = nonMainQueue.poll();
+                        poll.isInMainQueue = true;
+                        mainQueue.add(poll);
+                    }
+                    if (mainQueue.size() < k) {
+                        result.append("-1").append(" ");
+                        continue;
+                    }
+                } else {
+                    nonMainQueue.remove(value);
+                }
             }
-            final Queue<Integer> prQ = new PriorityQueue<>();
-            final Integer[] array = new Integer[r - l + 1];
-                    System.arraycopy(arr, l, array, 0, r - l + 1);
-            prQ.addAll(Arrays.asList(array));
-            int counter = 0;
-            while (counter < k - 1) {
-                prQ.poll();
-                counter++;
-            }
-            result.append(prQ.peek()).append(" ");
+            assert mainQueue.peek() != null;
+            result.append(mainQueue.peek().value).append(" ");
         }
         System.out.println(result);
         System.out.println();
+    }
+
+    private static class Value {
+        int index;
+        int value;
+        boolean isInMainQueue;
+
+        public Value(int index, int value, boolean isInMainQueue) {
+            this.index = index;
+            this.value = value;
+            this.isInMainQueue = isInMainQueue;
+        }
     }
 }
